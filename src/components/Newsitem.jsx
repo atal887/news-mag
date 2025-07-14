4000217833b63452e3898758f398d3fa74d3706e
Nandini Atal <nandiniatal@Nandinis-MacBook-Air.local>
2025-07-14 13:30:30 +0530

    ✅ Made site fully responsive

--- src/components/Newsitem.jsx
+++ src/components/Newsitem.jsx
@@ -2,8 +2,9 @@ import image from '../assets/news.jpg'
 const Newsitems = ({ title, description, src, url }) => {
     return (
 
-        <div className="card bg-dark text-light mb-3  my-3 p-3 shadow" style={{ maxWidth: "345px" }}>
-            <img src={src ? src : image} style={{ height: "200px", width: "300px" }} className="card-img-top" alt="..." />
+        <div className="card bg-dark text-light mb-3 w-100 shadow">
+
+            <img src={src ? src : image} className="card-img-top" alt="..." style={{ height: "200px", objectFit: "cover" }} />
             <div className="card-body">
                 <h5 className="card-title">{title.slice(0, 50)}</h5>
                 <p className="card-text">{description ? description.slice(0, 90) : "This is the latest news.It is information about something that has just happened."}</p>
